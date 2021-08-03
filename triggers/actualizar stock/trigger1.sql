b5e98610187d97f76869e1a2899534b652c4c579
issac <mojarraasesina21@gmail.com>
2021-08-03 12:17:18 -0500

    se agrego el archivo parael trigger

--- triggers/actualizar stock/trigger1.sql
+++ triggers/actualizar stock/trigger1.sql
@@ -1,7 +1,4 @@
-USE `proyecto1`;
-CREATE DEFINER = CURRENT_USER TRIGGER `DemoDos`.`restarStock` after INSERT 
-ON `carrito` FOR EACH ROW
+CREATE DEFINER = CURRENT_USER TRIGGER `proyecto1`.`actualisar_stock` AFTER INSERT ON `carrito` FOR EACH ROW
 BEGIN
-Update `productos` set `existencia` = `STexistenciaOCK` - `New.cantidad`
-where `id_producto` = `id_producto`;
-END
+	update productos set productos.id_producto
+END;
