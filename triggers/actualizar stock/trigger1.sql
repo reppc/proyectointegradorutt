6495bd50048688f2db42db2447753b7b92aa61ee
echodegrasa <74998947+echodegrasa@users.noreply.github.com>
2021-08-05 18:47:30 -0500

    se corrigieron errores en el archivo usuarios.sql

--- triggers/actualizar stock/trigger1.sql
+++ triggers/actualizar stock/trigger1.sql
@@ -3,5 +3,5 @@ TRIGGER `proyecto1`.`actualisar_stock` AFTER INSERT ON `carrito`
 FOR EACH ROW
 BEGIN
 	update productos set productos.existencia=producto.existencia - new.cantidad
-	where productos.id_producto=carrito.id_carrito
+	where productos.id_producto=new.id_producto
 END;
