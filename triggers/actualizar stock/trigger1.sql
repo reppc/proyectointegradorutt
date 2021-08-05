73ad66de12154dec9dc42f82fe2df9da279886f3
echodegrasa <74998947+echodegrasa@users.noreply.github.com>
2021-08-05 13:40:54 -0500

    avance a trigger y carga de inserciones

--- triggers/actualizar stock/trigger1.sql
+++ triggers/actualizar stock/trigger1.sql
@@ -1,4 +1,7 @@
-CREATE DEFINER = CURRENT_USER TRIGGER `proyecto1`.`actualisar_stock` AFTER INSERT ON `carrito` FOR EACH ROW
+CREATE DEFINER = CURRENT_USER 
+TRIGGER `proyecto1`.`actualisar_stock` AFTER INSERT ON `carrito` 
+FOR EACH ROW
 BEGIN
-	update productos set productos.id_producto
+	update productos set productos.existencia=producto.existencia - new.cantidad
+	where productos.id_producto=carrito.id_carrito
 END;
