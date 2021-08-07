8514d5468329836adff3cb46311c55232d572f51
reppc <84331772+reppc@users.noreply.github.com>
2021-08-06 19:14:36 -0500

    Add files via upload

---
+++ inicio/Interfaces-vistas/Blog/Proyecto/php/blog-consejos.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../styles/blog-conse.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<title>Blog-Consejos</title>
+</head>
+<body>
+	<div class="portada">
+		<div class="rectangulo">
+			<p class="textoR">Consejos</p>
+		</div>
+		<a href="blog-informativo.php" type="button" class="btn btn-primary"style="position: absolute;
+width: 150px;
+height: 38px;
+left: 24%;
+top: 150px;
+ font-size: 15px;
+ background-color: #6FD950;
+ color: black; #CBFDFA;#C4C4C4;
+ border-radius: 30px;
+">informativo </a>	
+<a href="blog-sugerencias.php" type="button" class="btn btn-primary"style="position: absolute;
+width: 150px;
+height: 38px;
+left: 44%;
+top: 150px;
+ font-size: 15px;
+ background-color:#CBFDFA;
+ color: black; #C4C4C4;
+ border-radius: 30px;
+">sugerencias </a>	
+<a href="blog-consejos.php" type="button" class="btn btn-primary"style="position: absolute;
+width: 150px;
+height: 38px;
+left: 64%;
+top: 150px;
+ font-size: 15px;
+ background-color:#C4C4C4;
+ color: black; 
+ border-radius: 30px;
+">Consejos</a>	
+
+	
+
+	</div>
+
+<div class="titulo1">Titulo 1</div>
+<p class="texto1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit <a href="">Ver más</a></p>
+ <img src="../img/img1.png" class="img1">
+ <div class="linea2"></div>
+  <div class="titulo2">Titulo 1</div>
+  <p class="texto2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit <a href="">Ver más</a></p>
+<img src="../img/img1.png" class="img2">
+<div class="linea1"></div>
+<div class="titulo3">Titulo 1</div>
+<p class="texto3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit <a href="">Ver más</a></p>
+<img src="../img/img1.png" class="img3">
+<div class="linea3"></div>
+<div class="titulo4">Titulo 1</div>
+<p class="texto4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit <a href="">Ver más</a></p>
+<img src="../img/img1.png" class="img4">
+<div class="titulo5">Titulo 1</div>
+<p class="texto5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit <a href="">Ver más</a></p>
+<img src="../img/img1.png" class="img5">
+<div class="titulo6">Titulo 1</div>
+<p class="texto6">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit <a href="">Ver más</a></p>
+<img src="../img/img1.png" class="img6">
+<div class="linea4"></div>
+</body>
+</html>
---
+++ inicio/Interfaces-vistas/Blog/Proyecto/php/blog2.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Blog-Inicio</title>
+	<link rel="stylesheet" type="text/css" href="../styles/Blog.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+</head>
+<body>
+	<div class="portada">
+		<div class="rectanguloportada">
+			<div class="seccion">Seccion Blog</div>
+			<figure class="imgportada"><img src="../img/portada.png" width="100%"></figure>
+			<div class="informes">Informes, preguntas y más...</div>
+		</div>
+		<div class="descripcion">
+			Técnicas y recursos para ayudarte con problematicas de software, hardware y otras cosas extras, asi donde tambien podras hacer tu propia cuestion en la seccion “preguntas”. Selecciona la que más te pueda ayudar:
+		</div>
+	</div>
+	<a href="blog-informativo.php" type="button" class="btn btn-primary" style="position: absolute;
+width: 300px;
+height: 75px;
+left: 123px;
+top: 690px;
+ font-size: 35px;
+ background-color: #6FD950;
+ color: black;#CBFDFA;#C4C4C4;
+ border-radius: 30px;">Informativo</a>
+	
+<a href="blog-sugerencias.php" type="button" class="btn btn-primary" style="position: absolute;
+width: 300px;
+height: 75px;
+left: 515px;
+top: 690px;
+ font-size: 35px;
+ background-color: #CBFDFA;
+ color: black;#C4C4C4;
+ border-radius: 30px;">Sugerencias</a>
+<a href="blog-consejos.php" type="button" class="btn btn-primary" style="position: absolute;
+width: 300px;
+height: 75px;
+left: 900px;
+top: 690px;
+ font-size: 35px;
+
+ background-color: #C4C4C4;
+ color: black;
+ border-radius: 30px;">Consejos</a>
+<!--Primeros dos anuncios-->
+	<div class="Anuncio1">
+		<img src="../img/anuncio1.png" class="imganuncio1">
+		<p class="Titulo1">Informativo</p>
+		<div style="
+position: absolute;
+width: 500px;
+height: 352px;
+left: 0px;
+top: 135px;
+background: linear-gradient(180deg, #DBFDCB 0%, #FFFFFF 100%);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.6);"
+>
+			<div class="descripcionanuncio1">
+				¿Cómo armar tu pc?: <br>
+				Algunos consejos  <br>y extras
+			</div>
+			<p class="Referencia1">Por:Administrador</p>
+			<button class="Botonanuncio1">
+				<p class="Vermas">Ver más..</p>
+			</button>
+		</div>
+	</div>
+
+	<div class="Anuncio2">
+			<img src="../img/anuncio2.png" class="imganuncio1">
+		<p class="Titulo1">Informativo</p>
+			<div style="
+position: absolute;
+width: 500px;
+height: 352px;
+left: 0px;
+top: 135px;
+background: linear-gradient(180deg, #DBFDCB 0%, #FFFFFF 100%);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.6);"
+>
+			<div class="descripcionanuncio1">
+				Problemas con virus: <br>
+					ideas de cómo <br> combatirlos...
+			</div>
+			<p class="Referencia1">Por:Administrador</p>
+			<button class="Botonanuncio1">
+				<p class="Vermas">Ver más..</p>
+			</button>
+		</div>
+	</div>
+	<!--Segundos tres anuncios-->
+	<div class="anuncio2">
+		<img src="../img/anuncio 3.png" class="imganuncio3">
+		<p class="titulo2">Sugerencias</p>
+		<div class="anunciocuadro2">
+			<p class="descripcion2">Solucionando <br> problemas con <br> los drivers:</p>
+			<p class="referencia2">Por:Administrador</p>
+			<button class="botonanuncio2">
+				<p class="vermas2">Ver más..</p>
+			</button>
+		</div>
+	</div>
+	<div class="anuncio2-2">
+		<img src="../img/anuncio4.png" class="imganuncio3">
+		<p class="titulo2">Sugerencias</p>
+		<div class="anunciocuadro2">
+			<p class="descripcion2">Problemas mas comunes: computadoras </p>
+			<p class="referencia2">Por:Administrador</p>
+			<button class="botonanuncio2">
+				<p class="vermas2">Ver más..</p>
+			</button>
+		</div>
+	</div>
+	<div class="anuncio2-3">
+		<img src="../img/anuncio5.png" class="imganuncio3">
+		<p class="titulo2">Sugerencias</p>
+		<div class="anunciocuadro2">
+			<p class="descripcion2">Mantenimiento <br>de una pc:  <br>por dentro... </p>
+			<p class="referencia2">Por:Administrador</p>
+			<button class="botonanuncio2">
+				<p class="vermas2">Ver más..</p>
+			</button>
+			
+		</div>
+	</div>
+	<div class="anuncio" style="position: absolute;
+width: 350px;
+height: 400px;
+left: 100px;
+top: 2250px;
+
+/* Oscuro - objetos */
+background: #2D2A2A;
+border-radius: 64px;">
+
+	<img src="../img/anuncio6.png" class="imganuncio3">
+	<p class="titulo2" style="left: 65px;">Consejos</p>
+		<div class="anunciocuadro3">
+			<p class="descripcion2">Mantenimiento <br>de una pc:  <br>por dentro... </p>
+			<p class="referencia2">Por:Administrador</p>
+			<button class="botonanuncio2">
+				<p class="vermas2">Ver más..</p>
+			</button>
+		</div>
+	</div>
+	<div class="anuncio" style="position: absolute;
+width: 350px;
+height: 400px;
+left: 500px;
+top: 2250px;
+
+/* Oscuro - objetos */
+background: #2D2A2A;
+border-radius: 64px;">
+
+	<img src="../img/anuncio7.png" class="imganuncio3">
+	<p class="titulo2" style="left: 65px;">Consejos</p>
+		<div class="anunciocuadro3">
+			<p class="descripcion2">Mantenimiento <br>de una pc:  <br>por dentro... </p>
+			<p class="referencia2">Por:Administrador</p>
+			<button class="botonanuncio2">
+				<p class="vermas2">Ver más..</p>
+			</button>
+		</div>
+	</div>
+	<div class="anuncio" style="position: absolute;
+width: 350px;
+height: 400px;
+left: 902px;
+top: 2250px;
+
+/* Oscuro - objetos */
+background: #2D2A2A;
+border-radius: 64px;">
+
+	<img src="../img/anuncio8.png" class="imganuncio3">
+	<p class="titulo2" style="left: 65px;">Consejos</p>
+		<div class="anunciocuadro3">
+			<p class="descripcion2">Mantenimiento <br>de una pc:  <br>por dentro... </p>
+			<p class="referencia2">Por:Administrador</p>
+			<button class="botonanuncio2">
+				<p class="vermas2">Ver más..</p>
+			</button>
+		</div>
+	</div>
+</body>
+</html>
