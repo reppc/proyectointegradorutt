13d50e909caaa7226b4b9f6bb2739812ccea7556
Neo-Brandon <84640666+Neo-Brandon@users.noreply.github.com>
2021-08-11 13:34:39 -0500

    Restauracion de inicios de sesion
    Ahora se puede iniciar sesion con normalidad

--- Scripts/database.php
+++ Scripts/database.php
@@ -1,10 +1,12 @@
+  
 <?php
     class Database
     {
+        private $basededatos="appsocompruebas";
         private $PDOLocal;
         private $user="root";
         private $password="";
-        private $server="mysql:host=localhost; dbname=appsocom";
+        private $server="mysql:host=localhost; dbname=appsocompruebas";
 
         function conectarDB()
         {
@@ -54,35 +56,60 @@
             try
             {
                 $pase=0;
-                $query="SELECT * FROM usuarios WHERE user='$usuario'";
+                $query="SELECT * FROM usuarios WHERE nombre_usuario='$usuario'";
                 $consulta=$this->PDOLocal->query($query);
                 while($renglon=$consulta->fetch(PDO::FETCH_ASSOC))
                 {
-                    if(password_verify($contra,$renglon['password']))
+                    if(password_verify($contra,$renglon['contraseña']))
                     {
                         $pase++;
                     }
+                    else{
+                        if($contra==$renglon['contraseña'])
+                        {
+                            $pase++;
+                        }
+                    }
                 }
+
+
 
                 if($pase>0)
                 {
                     session_start();
                     $_SESSION["usuario"]=$usuario;
-                    echo "<div class='alert alert-success'>";
-                    echo "<h2 align='center'>Bienvenido ".$_SESSION["usuario"]."</h2>";
-                    echo "</div>";
-                    header("refresh:2; ../index.php");
+                    echo "<div id='contenedor'>
+                    <div id='central'>
+                      <div id='login'>
+                        <div class='titulo'>
+                          Bienvenido <br> " . $_SESSION['usuario'] . "
+                        </div>
+                        <div class='img-fluid img-c'>
+                          <img class='img-tam' src='../login/imgLogin/checked-icon-clipart1.png'>
+                        </div>
+                      </div>
+                    </div>
+              </div>";
+        header("refresh:3; ../inicio/index copy.php");
+        
                 }
                 else{
                     echo "<div class='alert alert-danger'>";
                     echo "<h2 align='center'>Usuario o password incorrecto</h2>";
                     echo "</div>";
-                    header("refresh:2; ../php/FormLogin.php");
+                    header("refresh:2; ../login/login.php");
                 }
             }
             catch(PDOException $e){
                 echo $e->getMessage();
             }
+        }
+
+        function cerrarSesion()
+        {
+            session_start();
+            session_destroy();
+            header("Location:../inicio/index copy.php");
         }
     }
 ?>
